a9216a23a28387f43cd275512b097fde90d3c8f8
Bram Hoven <bram.hoven@hotmail.com>
2021-03-13 13:10:29 +0100

    Add typings

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { Client } from "./clients/Client";
+export { ApplicationClient } from "./clients/ApplicationClient";
+
+export { Options } from "./clients/interfaces/Options";
+export { Credentials } from "./clients/interfaces/Credentials";
